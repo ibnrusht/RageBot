05c6cb88ce3c224066c20a4e0387e32324661deb
ibnrusht <32167483+ibnrusht@users.noreply.github.com>
2017-09-24 16:28:52 +0300

    Added new feature "barrel_roll"

--- SQLighter.py
+++ SQLighter.py
@@ -22,6 +22,48 @@ class SQLighter:
             result = self.cursor.execute('SELECT * FROM music').fetchall()
             return len(result)
 
+    def check_user(self, user_id):
+        with self.connection:
+            return True if self.cursor.execute('SELECT * FROM users WHERE user_id = '+str(user_id)).fetchall() else False
+
+    def add_user(self, user_id, username, avatar_file_id):
+        with self.connection:
+            vls = [str(user_id), username, avatar_file_id]
+            self.cursor.execute('INSERT INTO users (user_id, username, avatar_file_id) VALUES (?, ?, ?)',vls)
+
+    def check_barrel(self,user_id):
+        """
+        Checking barrel_roll-gis existance
+        :param user_id: int, user_id
+        :return: bool
+        """
+        with self.connection:
+            tmp = self.cursor.execute('SELECT barrel_roll_id FROM users WHERE user_id = '+str(user_id)).fetchall()
+            tmp2 = tmp[0][0]
+            return True if tmp[0][0] else False
+
+    def add_barrel(self,user_id,barrel_roll_id):
+        """
+        Adding entry about barrel_roll-gif file id
+        :param user_id: int
+        :param barrel_roll_id: int
+        """
+        with self.connection:
+            vls = [barrel_roll_id, user_id]
+            self.cursor.execute('UPDATE users SET barrel_roll_id = ? WHERE user_id = ?', vls)
+
+    def get_barrel(self,user_id):
+        """
+        Getting barrel_roll-gif file id
+        :param user_id: int
+        :return: int, barrer-roll file id
+        """
+        with self.connection:
+            bar_id = self.cursor.execute('SELECT barrel_roll_id FROM users WHERE user_id = '+str(user_id)).fetchall()
+            return bar_id[0][0]
+
     def close(self):
         """ Закрываем текущее соединение с БД """
         self.connection.close()
+
+
